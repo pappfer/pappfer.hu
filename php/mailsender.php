bdda7f3b767fb3ed059564a022162954a471eb2e
Ferenc Papp <pappfer@gmail.com>
2018-01-05 11:09:23 +0100

    fixed email sending - using SMTP

--- php/mailsender.php
+++ php/mailsender.php
@@ -1,31 +1,28 @@
 <?php
 
-	$emailTo = "pappfer@pappfer.hu";
-	
-	$headers = "MIME-Version: 1.0\r\n";
-	$headers .= "Content-type: text/html; charset=utf-8\r\n";
-	$headers .= "From: ".$_POST['rsEmail']."\r\n";
-	
-	if (!isset($_POST['rsSubject'])) {
-		$subject = "Contact form message";
-	} else {
-		$subject = $_POST['rsSubject'];
-	}
-	
-	reset($_POST);
-	
-	$body = "";
-	$body .= "<p><b>Name: </b>".$_POST['rsName']."</p>";
-	$body .= "<p><b>Email: </b>".$_POST['rsEmail']."</p>";
-	$body .= "<p><b>Subject: </b>".$subject."</p>";
-	$body .= "<p><b>Message: </b>".$_POST['rsMessage']."</p>";	
-	
-	if( mail($emailTo, $subject, $body, $headers) ){
-		$mail_sent = true;
-	} else {
-		$mail_sent = false;
-	}	
-	if(!isset($resp)){
-		echo json_encode($mail_sent);
-	}
-?>
+require(__DIR__ . '/../vendor/autoload.php');
+
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
+$dotenv->load();
+
+$transport = (new Swift_SmtpTransport(getenv('SMTP_HOST'), intval(getenv('SMTP_PORT')), getenv('SMTP_SECURITY')))
+    ->setUsername(getenv('SMTP_USERNAME'))
+    ->setPassword(getenv('SMTP_PASSWORD'));
+
+$mailer = new Swift_Mailer($transport);
+
+if (!isset($_POST['rsSubject'])) {
+    $subject = "Contact form message";
+} else {
+    $subject = $_POST['rsSubject'];
+}
+
+$message = (new Swift_Message($subject))
+    ->setFrom([$_POST['rsEmail'] => $_POST['rsName']])
+    ->setTo([getenv('EMAIL')])
+    ->setReplyTo($_POST['rsEmail'])
+    ->setBody($_POST['rsMessage']);
+
+$result = $mailer->send($message);
+
+echo json_encode((bool)$result);
