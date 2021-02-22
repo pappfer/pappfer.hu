fa766d6011b165a9322a7d25184403d6242162a7
pappfer <pappfer@gmail.com>
2021-02-22 11:15:54 +0100

    Pagespeed optimizations.

--- gulpfile.js
+++ gulpfile.js
@@ -42,7 +42,7 @@ gulp.task('pack-js', function () {
 // concatenate and minify CSS files
 gulp.task('pack-css', function () {
   return gulp.src([
-    './fonts/fonts.css',
+    //'./fonts/fonts.css',
     './fonts/map-icons/css/map-icons.css',
     './fonts/icomoon/style.css',
     './js/plugins/jquery.bxslider/jquery.bxslider.css',
--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $testimonials = [
         'speech' => _('I would most definitely re-hire pappfer and I highly recommend his services.'),
     ],
 ];
-?> <!DOCTYPE html><html lang="<?= $lang ?>" class="theme-color-07cb79 theme-skin-light"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title><?= $title ?></title><link rel="shortcut icon" type="image/x-icon" href="<?= $root ?>favicon.ico"> <?php foreach (array_keys(validLangs()) as $langItem) {
+?> <!DOCTYPE html><html lang="<?= $lang ?>" class="theme-color-07cb79 theme-skin-light"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title><?= $title ?></title><link rel="shortcut icon" type="image/x-icon" href="<?= $root ?>favicon.ico"><link rel="preconnect" href="https://fonts.gstatic.com"><link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet"><link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet"> <?php foreach (array_keys(validLangs()) as $langItem) {
         if ($lang === $langItem) {
             continue;
         }
